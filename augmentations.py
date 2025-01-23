0f0ae5fe3723b0b3510a14c1b609e6cebc4740c0
mahdizmni <mahdizmnig@gmail.com>
2025-01-23 13:56:33 -0500

    loops for 100 times to find non-overlappping views.

--- augmentations.py
+++ augmentations.py
@@ -78,7 +78,7 @@ class TrainTransform(object):
         )
 
     def __call__(self, sample):
-        crop = RandomResizedCrop(100)
+        crop = RandomResizedCrop(224)
         v1, v2, c1, c2 = crop(sample)
         x1 = self.transform(v1)
         x2 = self.transform_prime(v2)
